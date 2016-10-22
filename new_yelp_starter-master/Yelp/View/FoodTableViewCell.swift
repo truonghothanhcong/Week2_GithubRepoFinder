58491e8b279ebd34dbd6db3f653d8fc1d658abb2
CongTruong <truonghothanhcong@gmail.com>
2016-10-22 22:13:25 +0700

    images fade

--- new_yelp_starter-master/Yelp/View/FoodTableViewCell.swift
+++ new_yelp_starter-master/Yelp/View/FoodTableViewCell.swift
@@ -22,7 +22,30 @@ class FoodTableViewCell: UITableViewCell {
     var business: Business! {
         didSet {
             if let imageURL = business.imageURL {
-                self.foodImageView.setImageWith(imageURL)
+                //self.foodImageView.setImageWith(imageURL)
+                let imageRequest = URLRequest(
+                    url: imageURL,
+                    cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                    timeoutInterval: 10)
+                foodImageView.setImageWith(
+                    imageRequest,
+                    placeholderImage: nil,
+                    success: { (imageRequest, imageResponse, image) -> Void in
+                        
+                        // imageResponse will be nil if the image is cached
+                        if imageResponse != nil {
+                            self.foodImageView.alpha = 0.0
+                            self.foodImageView.image = image
+                            UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                                self.foodImageView.alpha = 1.0
+                            })
+                        } else {
+                            self.foodImageView.image = image
+                        }
+                    },
+                    failure: { (imageRequest, imageResponse, error) -> Void in
+                        // do something for the failure condition
+                })
             }
             if let name = business.name {
                 self.foodNameLabel.text = name
